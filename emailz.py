b54ad4b15f915b4cddf863b55d84ad868227b170
PaulsKK <ainars.berzins@elbune.lv>
2023-04-19 12:44:22 +0300

    teksts

--- emailz.py
+++ emailz.py
@@ -1,18 +1,21 @@
 import smtplib
 
 def main():
+  sender_email="pauls.konov@gmail.com"
+  rec_email = "pauls.konov@gmail.com"
+  password = input(str("Ievadiet paroli : "))
+  message = "Emails No Pythona"
 
-sender_email = "pauls.konov@gmail.com"
-rec_email = "pauls.konov@gmail.com"
-password = input(str("Ievadiet paroli : "))
-message = "Emails No Pythona"
+  server = smtplib.SMTP('smtp.gmail.com', 587)
+  server.starttls()
+  server.login(sender_email, password)
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(sender_email, password)
+  print("Pierakstīšanās veiksmīga")
+  server.sendmail(sender_email, rec_email, message)
+  print("Emails ir nosūtīts uz ", rec_email)
 
-print("Pierakstīšanās veiksmīga")
-server.sendmail(sender_email, rec_email, message)
-print("Emails ir nosūtīts uz ", rec_email)
-if __name__ ==  "__main__"
-main()
+if __name__=='__main__':
+    main()
+
+
+    #vpbemywqlypkzdmz
